cc931c03c54bb5e96e0c00ca8e6419912f3df3b8
connor miller <ceddy9176@gmail.com>
2018-01-05 15:37:01 -0800

    second spec passed and second question works

--- js/caculator-interface.js
+++ js/caculator-interface.js
@@ -1,5 +1,6 @@
 import { Calculator } from './../js/calculator.js';
 
+  // year to seconds
   $(function() {
     $('#y-to-s').submit(function(event) {
       event.preventDefault();
@@ -7,5 +8,24 @@ import { Calculator } from './../js/calculator.js';
       let newCalculator = new Calculator();
       let output = newCalculator.ageInSeconds(ageInYear);
       $('#solution-1').text(output + " seconds");
+    });
+  });
+  //
+  $(function() {
+    $('#twoDatesSeconds').submit(function(event) {
+      event.preventDefault();
+      let year1 = $('#yearOne').val();
+      let month1 = $('#monthOne').val();
+      let day1 = $('#dayOne').val();
+      let date1 = [year1, month1, day1];
+
+      let year2 = $('#yearTwo').val();
+      let month2 = $('#monthTwo').val();
+      let day2 = $('#dayTwo').val();
+      let date2 = [year2, month2, day2];
+
+      let newCalculator = new Calculator();
+      let output = newCalculator.twoDatesSeconds(date2, date1);
+      $('#solution-2').text(output + " seconds");
     });
   });
--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -5,6 +5,13 @@ describe('CalculatorTests', function() {
   it('should calculate the age of someone into seconds', function() {
     let calculator = new Calculator();
     expect(calculator.ageInSeconds(19)).toEqual(599184000);
-
   });
+
+  it('should calculate the seconds between two dates', function() {
+    let calculator = new Calculator();
+    let date1 = [2018, 0, 1];
+    let date2 = [2018, 0, 2];
+    expect(calculator.twoDatesSeconds(date2,date1)).toEqual(86400);
+  });
+
 });
